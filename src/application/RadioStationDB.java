e57dc23f238876dc5306cf8717e76166be023112
RusMansur <rusdolg@gmail.com>
2022-12-24 13:43:46 +0300

    Оптимизация получения пути jar-файла

--- src/application/RadioStationDB.java
+++ src/application/RadioStationDB.java
@@ -14,79 +14,80 @@ import java.util.Map;
 import java.util.TreeMap;
 
 public class RadioStationDB {
-	static File radioListFile = new File("radiolist.csv").getAbsoluteFile();
+    static File radioListFile = new File(GetJarPath.getPath() + "/radiolist.csv");
+    TreeMap<String, RadioStation> radioStationDB = new TreeMap<>();
 
-	TreeMap<String, RadioStation> radioStationDB = new TreeMap<>();
+    public void createStationsListFile() {
+        if (!radioListFile.exists()) {
+            try {
+                radioListFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-	public void createStationsListFile() {
-		if (!radioListFile.exists()) {
-			try {
-				radioListFile.createNewFile();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+    TreeMap<String, RadioStation> readStationsFromFile() {
+        try (CSVReader reader = new CSVReader(new FileReader(radioListFile))) {
+            String[] field;
+            while (true) {
+                if ((field = reader.readNext()) != null) {
+                    RadioStation radioStation = new RadioStation(
+                            field[0],
+                            field[1],
+                            field[2]
+                    );
+                    radioStationDB.put(radioStation.getName(), radioStation);
+                } else {
+                    break;
+                }
+            }
+        } catch (IOException | CsvValidationException e) {
+            e.printStackTrace();
+        }
+        return radioStationDB;
+    }
 
-	TreeMap<String, RadioStation> readStationsFromFile() {
-		try (CSVReader reader = new CSVReader(new FileReader(radioListFile))) {
-			String[] field;
-			while (true) {
-				if ((field = reader.readNext()) != null) {
-					RadioStation radioStation = new RadioStation(
-							field[0],
-							field[1],
-							field[2]
-					);
-					radioStationDB.put(radioStation.getName(), radioStation);
-				} else {break;}
-			}
-		} catch (IOException | CsvValidationException e) {
-			e.printStackTrace();
-		}
-		return radioStationDB;
-	}
+    void saveStationToFile(RadioStation radioStation) {
+        try (CSVWriter csvWriter = new CSVWriter(new FileWriter(radioListFile, true))) {
+            String[] station = {
+                    radioStation.getName(),
+                    radioStation.getLogoPath(),
+                    radioStation.getUrl()
+            };
+            csvWriter.writeNext(station);
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
 
-	void saveStationToFile(RadioStation radioStation) {
-		try (CSVWriter csvWriter = new CSVWriter(new FileWriter(radioListFile, true))) {
-			String[] station = {
-					radioStation.getName(),
-					radioStation.getLogoPath(),
-					radioStation.getUrl()
-			};
-			csvWriter.writeNext(station);
-		} catch (IOException exception) {
-			exception.printStackTrace();
-		}
-	}
+    void saveListOfStationsToFile(TreeMap<String, RadioStation> radioStationList) {
+        try (CSVWriter csvWriter = new CSVWriter(new FileWriter(radioListFile))) {
+            for (Map.Entry<String, RadioStation> entry : radioStationList.entrySet()) {
+                String[] fields = {
+                        entry.getKey(),
+                        entry.getValue().getLogoPath(),
+                        entry.getValue().getUrl()
+                };
+                csvWriter.writeNext(fields);
+            }
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
 
-	void saveListOfStationsToFile(TreeMap<String, RadioStation> radioStationList) {
-		try (CSVWriter csvWriter = new CSVWriter(new FileWriter(radioListFile))) {
-			for (Map.Entry<String, RadioStation> entry : radioStationList.entrySet()) {
-				String[] fields = {
-						entry.getKey(),
-						entry.getValue().getLogoPath(),
-						entry.getValue().getUrl()
-				};
-				csvWriter.writeNext(fields);
-			}
-		} catch (IOException exception) {
-			exception.printStackTrace();
-		}
-	}
+    ObservableList<String> nameList() {
+        ObservableList<String> nameList = FXCollections.observableArrayList();
+        for (Map.Entry<String, RadioStation> entry : radioStationDB.entrySet()) {
+            nameList.add(entry.getKey());
+        }
+        return nameList;
+    }
 
-	ObservableList<String> nameList() {
-		ObservableList<String> nameList = FXCollections.observableArrayList();
-		for (Map.Entry<String, RadioStation> entry : radioStationDB.entrySet()) {
-			nameList.add(entry.getKey());
-		}
-		return nameList;
-	}
-
-	RadioStation getRadioStation(String name) {
-		for (Map.Entry<String, RadioStation> entry : radioStationDB.entrySet()) {
-			if (entry.getKey().equals(name)) return entry.getValue();
-		}
-		return null;
-	}
+    RadioStation getRadioStation(String name) {
+        for (Map.Entry<String, RadioStation> entry : radioStationDB.entrySet()) {
+            if (entry.getKey().equals(name)) return entry.getValue();
+        }
+        return null;
+    }
 }
