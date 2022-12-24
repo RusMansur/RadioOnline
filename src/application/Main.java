1984383766e6ba3a79cb80ccbea9f7d4969e3dc2
RusMansur <rusdolg@gmail.com>
2022-12-24 23:02:53 +0300

    Добавлен значок для док-панели macOS

--- src/application/Main.java
+++ src/application/Main.java
@@ -3,42 +3,50 @@ package application;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
-import java.util.Objects;
+import java.awt.*;
 
 public class Main extends Application {
+    private double xOffset;
+    private double yOffset;
 
-	private double xOffset;
-	private double yOffset;
+    public static void main(String[] args) {
+        Application.launch();
+    }
 
-	public static void main(String[] args) {
-		Application.launch();
-	}
+    @Override
+    public void start(Stage stage) throws Exception {
+// Значок приложения в док-панели macOS
+        if (Taskbar.isTaskbarSupported()) {
+            var taskbar = Taskbar.getTaskbar();
 
-	@Override
-	public void start(Stage stage) throws Exception {
-		stage.getIcons().add(new Image(Objects.requireNonNull(Main.class.getResourceAsStream("/resources/images/Radio.png"))));
-		FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/resources/mainWindow.fxml"));
-		VBox root = fxmlLoader.load();
-		MainController controller = fxmlLoader.getController();
-		controller.setPrimaryStage(stage);
-		root.setOnMousePressed(event -> {
-			xOffset = stage.getX() - event.getScreenX();
-			yOffset = stage.getY() - event.getScreenY();
-		});
-		root.setOnMouseDragged(event -> {
-			stage.setX(event.getScreenX() + xOffset);
-			stage.setY(event.getScreenY() + yOffset);
-		});
-		stage.setResizable(false);
-		stage.initStyle(StageStyle.UNDECORATED);
-		stage.initStyle(StageStyle.TRANSPARENT);
-		stage.setScene(new Scene(root, Color.TRANSPARENT));
-		stage.show();
-	}
+            if (taskbar.isSupported(Taskbar.Feature.ICON_IMAGE)) {
+                final Toolkit defaultToolkit = Toolkit.getDefaultToolkit();
+                var dockIcon = defaultToolkit.getImage(getClass().getResource("/resources/images/Radio.png"));
+                taskbar.setIconImage(dockIcon);
+            }
+        }
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/resources/mainWindow.fxml"));
+
+        VBox root = fxmlLoader.load();
+        MainController controller = fxmlLoader.getController();
+        controller.setPrimaryStage(stage);
+        root.setOnMousePressed(event -> { // получение координат при нажатии мыши на окне
+            xOffset = stage.getX() - event.getScreenX();
+            yOffset = stage.getY() - event.getScreenY();
+        });
+        root.setOnMouseDragged(event -> { // установка новых координат при перетаскивании окна
+            stage.setX(event.getScreenX() + xOffset);
+            stage.setY(event.getScreenY() + yOffset);
+        });
+        stage.setResizable(false);
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.initStyle(StageStyle.TRANSPARENT);
+        stage.setScene(new Scene(root, Color.TRANSPARENT));
+        stage.show();
+    }
 }
--- src/module-info.java
+++ src/module-info.java
@@ -1,4 +1,4 @@
-module AudioFX {
+module RadioOnline {
 	opens application to javafx.graphics, javafx.controls, javafx.fxml;
 	requires javafx.graphics;
 	requires javafx.controls;
@@ -6,4 +6,5 @@ module AudioFX {
 	requires javafx.media;
 	requires com.opencsv;
     requires lombok;
+	requires java.desktop;
 }
